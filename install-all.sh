b193750da68d258526a24a2527a4e440dde15872
Richard de Wit <henk.exe@gmail.com>
2019-09-13 10:51:44 +0200

    Add ideavimrc

--- install-all.sh
+++ install-all.sh
@@ -2,6 +2,8 @@
 
 folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+ln -sf $folder/ideavimrc $HOME/.ideavimrc
+
 bash $folder/Emacs/install.sh
 bash $folder/Vim/install.sh
 bash $folder/VSCode/install.sh
