abd083a7a96c0ad8ace9995f5d92b8dd159c54c5
Richard de Wit <henk.exe@gmail.com>
2020-02-24 09:36:44 +0100

    Fix Vim dirs

--- Vim/install.sh
+++ Vim/install.sh
@@ -2,8 +2,7 @@
 
 folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-[ -d $HOME/.vim ] || mkdir -p $HOME/.vim
-[ -d $HOME/.nvim ] || mkdir -p $HOME/.nvim
+mkdir -p $HOME/.{n,}vim/{backup,swap,undo,files/info}
 [ -d $HOME/.config/nvim ] || mkdir -p $HOME/.config/nvim
 ln -sf $folder/vimrc $HOME/.vimrc
 ln -sf $folder/nvim_init.vim $HOME/.config/nvim/init.vim
@@ -12,10 +11,6 @@ ln -sf $folder/nvim_init.vim $HOME/.config/nvim/init.vim
 curl -fLo $HOME/.vim/autoload/plug.vim         --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# vim-startify
-mkdir -p $HOME/.vim/files/info
-mkdir -p $HOME/.nvim/{backup,swap,undo,files/info}
-
-vim +PlugInstall +qall
-nvim +PlugInstall +qall
+\vim +PlugInstall +qall
+\nvim +PlugInstall +qall
 
