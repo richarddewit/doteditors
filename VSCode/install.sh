1bead61ebab362135a4dc666b060820859c85d38
Richard de Wit <richard@komma.nl>
2023-06-15 13:51:17 +0200

    Fix path for MacOS and fix folders with spaces
    Shame, shame, shame (for the second part)

--- VSCode/install.sh
+++ VSCode/install.sh
@@ -2,13 +2,16 @@
 
 folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 vscode_folder="$HOME/.config/Code/User"
+if [ "$PLATFORM" = "macos" ]; then
+  vscode_folder="$HOME/Library/Application Support/Code/User"
+fi
 
-rm -r $vscode_folder/settings.json
-rm -r $vscode_folder/keybindings.json
-rm -r $vscode_folder/snippets
-mkdir -p $vscode_folder
-ln -sf $folder/settings.json $vscode_folder/settings.json
-ln -sf $folder/keybindings.json $vscode_folder/keybindings.json
-ln -sf $folder/snippets $vscode_folder/snippets
+rm -r "$vscode_folder/settings.json"
+rm -r "$vscode_folder/keybindings.json"
+rm -r "$vscode_folder/snippets"
+mkdir -p "$vscode_folder"
+ln -sf "$folder/settings.json" "$vscode_folder/settings.json"
+ln -sf "$folder/keybindings.json" "$vscode_folder/keybindings.json"
+ln -sf "$folder/snippets" "$vscode_folder/snippets"
 echo
 echo "Run install_extensions.sh to.. install extensions :)"
